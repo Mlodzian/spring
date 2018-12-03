bfe7ea9204d834d0f1dfa538a3ebfeaa77ece531
Michal Cwiekala <michal.cwiekala@ista.pl>
2018-12-03 17:40:53 +0100

    init commit

--- src/test/java/com/michal/demo/DemoApplicationTests.java
+++ src/test/java/com/michal/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package pl.spring.tutorial;
+package com.michal.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TutorialApplicationTests {
+public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
